6db27a22c7e6d9beb45cf7c16ec6e3ce75d35fe6
lazyboismile <lazyboijon@gmail.com>
2025-02-22 05:08:55 +0500

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ console.log("Web Server is running");
 const express = require('express');
 const app = express();
 const http = require('http');
+const fs = require('fs');
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("Error reading user.json", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
+
 
 // 1  Introduction 
 app.use(express.static("public"));
@@ -18,6 +29,9 @@ app.post("/create-item", (req, res) => {
     console.log(req);
     res.json("Item created successfully");
 });
+app.get("/author", (req, res) => {
+    res.render("author", {user: user});
+})
 app.get("/",  function (req, res) {
     res.render("harid");
 });
