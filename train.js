bb9d3a7613e9cb50bd15c87a28b2a708f574ef3a
lazyboismile <122263359+lazyboismile@users.noreply.github.com>
2025-03-05 23:12:50 +0500

    D task

--- train.js
+++ train.js
@@ -17,10 +17,10 @@ const list = [
 function checkContent(str1, str2) {
     if (str1.length !== str2.length) return false;
 
-    const sorted1 = str1.split('').sort().join('');
-    const sorted2 = str2.split('').sort().join('');
+    const result1 = str1.split('').sort().join('');
+    const result2 = str2.split('').sort().join('');
 
-    return sorted1 === sorted2;
+    return result1 === result2;
 }
 
 // Test qib kuramiza
