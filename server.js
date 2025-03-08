43d1b6ab8eb419c561cd5c92087f77350fc1da39
lazyboismile <122263359+lazyboismile@users.noreply.github.com>
2025-03-08 14:31:14 +0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongodb.connect(connectionString, {
         
         const app = require('./app');
         const server = http.createServer(app);
-        let PORT = 5009;
+        let PORT = 5011;
         server.listen(PORT, function() {
             console.log(`Server is running on port: ${PORT}, http://localhost:${PORT}`);
         });
